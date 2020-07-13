c2b85a21c7a1c3dd8b10deb56a2c2f4647e809b9
DulminiD <dulminid1107@gmail.com>
2020-07-14 00:05:45 +0530

    all

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist/my-ng8-app'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'src'}
+);
+});
+app.listen(process.env.PORT || 8080);
+
+a
